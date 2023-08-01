4e250dca76cce1f2cbc5c71bb0618698c3220dd4
arenasys <79061310+arenasys@users.noreply.github.com>
2023-08-01 18:34:52 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 import gradio as gr
 from datetime import datetime
 
+from modules import shared
+
 params = {
     "display_name": ""
 }
@@ -28,7 +30,7 @@ def output_modifier(string, state):
 
 def select(selected_label):
     if not HISTORY:
-        return "", "", gr.update(choices=[], value="")
+        return "...", "...", gr.update(choices=["None"], value="None")
 
     label, output_text, input_text = HISTORY[-1]
     labels = [i[0] for i in HISTORY][::-1]
@@ -37,21 +39,33 @@ def select(selected_label):
         if entry[0] == selected_label:
             label, output_text, input_text = entry
             break
+    
+    output_text, input_text = output_text.strip(), input_text.strip()
 
     return output_text, input_text, gr.update(choices=labels, value=label)
+
+def clear(selected_label):
+    global HISTORY
+    HISTORY = []
+    return select(selected_label)
 
 def ui():
     with gr.Tab("History"):
         with gr.Row().style(equal_height=True):
-            dropdown = gr.Dropdown([], show_label=False, elem_classes='slim-dropdown')
-            reload = gr.Button("Reload", elem_classes='refresh-button')
+            clear_btn = gr.Button("Clear", elem_classes='refresh-button')
+            dropdown = gr.Dropdown(["None"], value="None", show_label=False, elem_classes='slim-dropdown')
+            reload_btn = gr.Button("Reload", elem_classes='refresh-button')
         with gr.Column():
             with gr.Tab("Output"):
-                output_area = gr.TextArea("", show_label=False)
+                output_area = gr.TextArea("...", show_label=False)
             with gr.Tab("Input"):
-                input_area = gr.TextArea("", show_label=False)
+                input_area = gr.TextArea("...", show_label=False)
 
     args = ([dropdown], [output_area, input_area, dropdown])
+    kwargs = {'show_progress': False, 'queue': False}
 
-    reload.click(select, *args)
-    dropdown.change(select, *args)
+    shared.gradio['Generate'].click(select, *args, **kwargs)
+
+    clear_btn.click(clear, *args, **kwargs)
+    reload_btn.click(select, *args, **kwargs)
+    dropdown.change(select, *args, **kwargs)
